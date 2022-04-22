ad88c8963c6dd9152124ba650c6937171f7992c3
DESKTOP-ODVG4OU\Kevin <kevponce420@gmail.com>
2022-04-22 17:21:53 -0600

    Ordenes listas

---
+++ class/class-empresas.php
@@ -0,0 +1,133 @@
+<?php 
+
+    class Empresas{
+
+      Private  $idEmpresa;
+      Private   $nombreEmpresa;
+      Private   $baner;
+      Private   $logo;
+      Private   $calificacion;
+      Private  $productos;
+
+      /**
+       * Get the value of idEmpresa
+       */ 
+      public function getIdEmpresa()
+      {
+            return $this->idEmpresa;
+      }
+
+      /**
+       * Set the value of idEmpresa
+       *
+       * @return  self
+       */ 
+      public function setIdEmpresa($idEmpresa)
+      {
+            $this->idEmpresa = $idEmpresa;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of nombreEmpresa
+       */ 
+      public function getNombreEmpresa()
+      {
+            return $this->nombreEmpresa;
+      }
+
+      /**
+       * Set the value of nombreEmpresa
+       *
+       * @return  self
+       */ 
+      public function setNombreEmpresa($nombreEmpresa)
+      {
+            $this->nombreEmpresa = $nombreEmpresa;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of baner
+       */ 
+      public function getBaner()
+      {
+            return $this->baner;
+      }
+
+      /**
+       * Set the value of baner
+       *
+       * @return  self
+       */ 
+      public function setBaner($baner)
+      {
+            $this->baner = $baner;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of logo
+       */ 
+      public function getLogo()
+      {
+            return $this->logo;
+      }
+
+      /**
+       * Set the value of logo
+       *
+       * @return  self
+       */ 
+      public function setLogo($logo)
+      {
+            $this->logo = $logo;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of calificacion
+       */ 
+      public function getCalificacion()
+      {
+            return $this->calificacion;
+      }
+
+      /**
+       * Set the value of calificacion
+       *
+       * @return  self
+       */ 
+      public function setCalificacion($calificacion)
+      {
+            $this->calificacion = $calificacion;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of productos
+       */ 
+      public function getProductos()
+      {
+            return $this->productos;
+      }
+
+      /**
+       * Set the value of productos
+       *
+       * @return  self
+       */ 
+      public function setProductos($productos)
+      {
+            $this->productos = $productos;
+
+            return $this;
+      }
+    }
+
+?>
---
+++ class/class-motoristas.php
@@ -0,0 +1,197 @@
+<?php 
+    class Motoristas{
+
+     Private   $idMotorista;
+     Private    $nombreMotorista;
+     Private    $apellidoMotorista;
+     Private   $correo;
+     Private   $contraseña;
+     Private   $ordenesTomadas;
+
+
+        public function __construct(
+         $idMotorista,
+        $nombreMotorista,
+        $apellidoMotorista,
+       $correo,
+       $contraseña,
+       $ordenesTomadas,
+        )
+        {
+            $this->  idMotorista->  $idMotorista;
+            $this->  nombreMotorista->   $nombreMotorista;
+            $this-> apellidoMotorista-> $apellidoMotorista;
+            $this-> correo ->  $correo;
+            $this-> contraseña -> $contraseña;
+            $this->ordenesTomadas->  $ordenesTomadas;
+
+        }
+
+        public static function obtenerMotoristas(){
+            $contenidoArchivo= file_get_contents('../data/motoristas.json');
+            echo $contenidoArchivo;
+
+        }
+
+        public static function obtenerMotorista($id){
+
+            $contenidoArchivo=file_get_contents('../data/motoristas.json');
+            $motoristas=json_decode($contenidoArchivo,true);
+            $motorista=null;
+
+            for ($i=0; $i < sizeof($motoristas); $i++) { 
+                    if($motoristas[$i]["idMotorista"]==$id){
+                        $motorista=$motoristas[$i];
+                        break;
+                    }
+
+            }
+            echo json_encode($motorista);
+        }
+
+
+        public function guardarMotorista(){
+
+            $contenidoArchivoMotoristas=file_get_contents('../data/motoristas.json');
+            $motoristas=json_decode($contenidoArchivoMotoristas,true);
+
+            $motoristas[]=array(
+        "idMotorista"=>$this->idMotrista,
+        "nombreMotorista"=>$this->nombreMotorista,
+        "apellidoMotorista"=>$this->apellidoMotorista,
+        "correo"=>$this->correo,
+        "contraseña"=>$this->contraseña,
+        
+
+            );
+            $arhivo=fopen('../data/motoristas.json','w');
+            fwrite($arhivo,json_encode($motoristas));
+            fclose($arhivo);
+            echo '{"codigoResultado":1, "mensaje":"Motorista guardado con exito"}';
+
+        }
+
+     /**
+      * Get the value of idMotorista
+      */ 
+     public function getIdMotorista()
+     {
+          return $this->idMotorista;
+     }
+
+     /**
+      * Set the value of idMotorista
+      *
+      * @return  self
+      */ 
+     public function setIdMotorista($idMotorista)
+     {
+          $this->idMotorista = $idMotorista;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of nombreMotorista
+      */ 
+     public function getNombreMotorista()
+     {
+          return $this->nombreMotorista;
+     }
+
+     /**
+      * Set the value of nombreMotorista
+      *
+      * @return  self
+      */ 
+     public function setNombreMotorista($nombreMotorista)
+     {
+          $this->nombreMotorista = $nombreMotorista;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of apellidoMotorista
+      */ 
+     public function getApellidoMotorista()
+     {
+          return $this->apellidoMotorista;
+     }
+
+     /**
+      * Set the value of apellidoMotorista
+      *
+      * @return  self
+      */ 
+     public function setApellidoMotorista($apellidoMotorista)
+     {
+          $this->apellidoMotorista = $apellidoMotorista;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of correo
+      */ 
+     public function getCorreo()
+     {
+          return $this->correo;
+     }
+
+     /**
+      * Set the value of correo
+      *
+      * @return  self
+      */ 
+     public function setCorreo($correo)
+     {
+          $this->correo = $correo;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of contraseña
+      */ 
+     public function getContraseña()
+     {
+          return $this->contraseña;
+     }
+
+     /**
+      * Set the value of contraseña
+      *
+      * @return  self
+      */ 
+     public function setContraseña($contraseña)
+     {
+          $this->contraseña = $contraseña;
+
+          return $this;
+     }
+
+     /**
+      * Get the value of ordenesTomadas
+      */ 
+     public function getOrdenesTomadas()
+     {
+          return $this->ordenesTomadas;
+     }
+
+     /**
+      * Set the value of ordenesTomadas
+      *
+      * @return  self
+      */ 
+     public function setOrdenesTomadas($ordenesTomadas)
+     {
+          $this->ordenesTomadas = $ordenesTomadas;
+
+          return $this;
+     }
+    }
+
+
+
+?>
